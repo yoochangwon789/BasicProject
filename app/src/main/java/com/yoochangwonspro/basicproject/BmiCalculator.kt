46eaefc8a492b4105d0a5b9d11512e59a6345993
CN <ckddnjs554@naver.com>
2021-07-22 01:26:27 +0900

    Feat : BmiCalculator result btn 클릭시 예외 처리 작성과 변수 설정

--- app/src/main/java/com/yoochangwonspro/basicproject/BmiCalculator.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/BmiCalculator.kt
@@ -2,10 +2,26 @@ package com.yoochangwonspro.basicproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_bmi_calculator.*
 
 class BmiCalculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi_calculator)
+
+        result_btn.setOnClickListener {
+            // 값이 빈경우
+            if (height_edit_text.text.isEmpty() || weight_edit_text.text.isEmpty()) {
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_LONG).show()
+                // @ 뒤의 함수를 return 하겠다
+                return@setOnClickListener
+            }
+
+            // 이 아래로는 빈값이 올 수 없음
+            val height: Int = (height_edit_text.text.toString()).toInt()
+            val weight: Int = (weight_edit_text.text.toString()).toInt()
+
+        }
     }
 }
