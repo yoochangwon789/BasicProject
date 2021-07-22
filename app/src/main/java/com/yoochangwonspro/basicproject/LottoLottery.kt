4609d8a847ffb77e92b32012a14a04d3af220979
CN <ckddnjs554@naver.com>
2021-07-22 18:30:03 +0900

    Feat : Lotto 레이아웃 디자인 구성

---
+++ app/src/main/java/com/yoochangwonspro/basicproject/LottoLottery.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.basicproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LottoLottery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lotto_lottery)
+    }
+}
