33a26e4a745b3c90044226d063692818a7fc7554
CN <ckddnjs554@naver.com>
2021-07-24 17:53:11 +0900

    Feat : SecretDiaryActivity openButton, changePasswordButton lazy 변수 타입 추가 작업 및 numberPicker onCreate 변수 설정 작업

--- app/src/main/java/com/yoochangwonspro/basicproject/SecretDiaryActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/SecretDiaryActivity.kt
@@ -3,6 +3,7 @@ package com.yoochangwonspro.basicproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.NumberPicker
+import androidx.appcompat.widget.AppCompatButton
 
 class SecretDiaryActivity : AppCompatActivity() {
 
@@ -30,8 +31,22 @@ class SecretDiaryActivity : AppCompatActivity() {
             }
     }
 
+    private val openButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.open_btn)
+    }
+
+    private val changePasswordButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.change_password_btn)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_secret_diary)
+
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
+
     }
 }
