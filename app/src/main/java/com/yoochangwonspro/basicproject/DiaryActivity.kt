1f3c13af1d64a4de04c185f861be6e95329de7fc
CN <ckddnjs554@naver.com>
2021-07-25 16:19:51 +0900

    Feat : DiaryActivity thread 생성 및 handler 설정

--- app/src/main/java/com/yoochangwonspro/basicproject/DiaryActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/DiaryActivity.kt
@@ -3,6 +3,8 @@ package com.yoochangwonspro.basicproject
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.EditText
 import androidx.core.content.edit
 import androidx.core.widget.addTextChangedListener
@@ -13,6 +15,9 @@ class DiaryActivity : AppCompatActivity() {
         findViewById(R.id.diary_edit_text)
     }
 
+    // 메인 쓰레드에 연결돼 있는 Handler
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
@@ -20,12 +25,17 @@ class DiaryActivity : AppCompatActivity() {
         val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
 
         diaryEditText.setText(detailPreferences.getString("detail", ""))
+
+        val runnable = Runnable {
+            getSharedPreferences("diary", Context.MODE_PRIVATE).edit {
+                putString("detail", diaryEditText.text.toString())
+            }
+        }
+
         //  text 가 변경 될 때 마다 이 함수가 호출
         // 변경 될때마다 저장시킬 수 있도록 한다
         diaryEditText.addTextChangedListener {
-            detailPreferences.edit {
-                putString("detail", diaryEditText.text.toString())
-            }
+            
         }
     }
 }
