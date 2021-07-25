105991befa2385839e8764192fb55881a25a6a2d
CN <ckddnjs554@naver.com>
2021-07-25 16:03:19 +0900

    Feat : DiaryActivity text 가 변경 될 때 마다 저장 되는 기능 추가

--- app/src/main/java/com/yoochangwonspro/basicproject/DiaryActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/DiaryActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import androidx.core.content.edit
+import androidx.core.widget.addTextChangedListener
 
 class DiaryActivity : AppCompatActivity() {
 
@@ -17,5 +19,13 @@ class DiaryActivity : AppCompatActivity() {
 
         val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
 
+        diaryEditText.setText(detailPreferences.getString("detail", ""))
+        //  text 가 변경 될 때 마다 이 함수가 호출
+        // 변경 될때마다 저장시킬 수 있도록 한다
+        diaryEditText.addTextChangedListener {
+            detailPreferences.edit {
+                putString("detail", diaryEditText.text.toString())
+            }
+        }
     }
 }
