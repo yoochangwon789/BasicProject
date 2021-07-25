22ddb6c7569d9c8c54460428a77234619a687b55
CN <ckddnjs554@naver.com>
2021-07-25 15:56:52 +0900

    Feat : DiaryActivity editText id 연결 작업

--- app/src/main/java/com/yoochangwonspro/basicproject/DiaryActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/DiaryActivity.kt
@@ -1,11 +1,21 @@
 package com.yoochangwonspro.basicproject
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class DiaryActivity : AppCompatActivity() {
+
+    private val diaryEditText: EditText by lazy {
+        findViewById(R.id.diary_edit_text)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
+
+        val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
+
     }
 }
