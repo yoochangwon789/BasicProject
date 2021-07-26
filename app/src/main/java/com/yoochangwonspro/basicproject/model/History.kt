531976744ccf149a9e94da25ac9d6d807973540d
CN <ckddnjs554@naver.com>
2021-07-26 22:22:29 +0900

    Feat : History data class 생성 및 Entity 설정

--- app/src/main/java/com/yoochangwonspro/basicproject/model/History.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/model/History.kt
@@ -1,8 +1,13 @@
 package com.yoochangwonspro.basicproject.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 // DB Table 로 사용
+@Entity
 data class History(
-    val uid: Int?,                  // Primary key
-    val expression: String?,        // 계산식
-    val result: String?             // 결과
+    @PrimaryKey val uid: Int?,                                       // Primary key
+    @ColumnInfo(name = "expression") val expression: String?,        // 계산식
+    @ColumnInfo(name = "result") val result: String?                 // 결과
 )
