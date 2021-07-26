23b723cba37b85f15c6d3d9ede077a0ba9b83951
CN <ckddnjs554@naver.com>
2021-07-26 18:38:35 +0900

    Feat : CalculatorActivity buttonClicked 클릭시 numberButtonClicked 함수 호출 및 number 의 String 타입으로 값을 넣어준다

--- app/src/main/java/com/yoochangwonspro/basicproject/CalculatorActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/CalculatorActivity.kt
@@ -3,6 +3,7 @@ package com.yoochangwonspro.basicproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,21 @@ class CalculatorActivity : AppCompatActivity() {
     }
 
     fun buttonClicked(v: View) {
+        when (v.id) {
+            R.id.zero_btn -> numberButtonClicked("0")
+            R.id.one_btn -> numberButtonClicked("1")
+            R.id.two_btn -> numberButtonClicked("2")
+            R.id.three_btn -> numberButtonClicked("3")
+            R.id.four_btn -> numberButtonClicked("4")
+            R.id.five_btn -> numberButtonClicked("5")
+            R.id.six_btn -> numberButtonClicked("6")
+            R.id.seven_btn -> numberButtonClicked("7")
+            R.id.eight_btn -> numberButtonClicked("8")
+            R.id.nine_btn -> numberButtonClicked("9")
+        }
+    }
+
+    private fun numberButtonClicked(number: String) {
 
     }
 
