d7dbe4627b462949daad230983ddae35a2f22a0e
CN <ckddnjs554@naver.com>
2021-07-26 18:33:10 +0900

    Feat : CalculatorActivity onClick 메더스 설정 초기 함수 구현

--- app/src/main/java/com/yoochangwonspro/basicproject/CalculatorActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/CalculatorActivity.kt
@@ -2,10 +2,27 @@ package com.yoochangwonspro.basicproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+    }
+
+    fun buttonClicked(v: View) {
+
+    }
+
+    fun historyButtonClicked(v: View) {
+
+    }
+
+    fun clearButtonClicked(v: View) {
+
+    }
+
+    fun resultButtonClicked(v: View) {
+
     }
 }
