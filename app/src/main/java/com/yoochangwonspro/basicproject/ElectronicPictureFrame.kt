e2a5efde7f7150dbddf69bc1fa875076cbd66424
CN <ckddnjs554@naver.com>
2021-07-28 19:47:03 +0900

    Feat : ElectronicPictureFrame Button 전역 변수 추가

--- app/src/main/java/com/yoochangwonspro/basicproject/ElectronicPictureFrame.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/ElectronicPictureFrame.kt
@@ -2,8 +2,18 @@ package com.yoochangwonspro.basicproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ElectronicPictureFrame : AppCompatActivity() {
+
+    private val addPhotoButton: Button by lazy {
+        findViewById(R.id.add_photo_btn)
+    }
+
+    private val startPhotoFrameModeButton: Button by lazy {
+        findViewById(R.id.start_photo_frame_mode_btn)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_electronic_picture_frame)
