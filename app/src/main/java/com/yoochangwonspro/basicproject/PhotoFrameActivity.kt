5ede236ad699f9b5e7bda37cc6eabaf5f0ef00e8
CN <ckddnjs554@naver.com>
2021-07-28 21:34:26 +0900

    Feat : PhotoFrameActivity intent 를 통해서 데이터 get 부분구현

--- app/src/main/java/com/yoochangwonspro/basicproject/PhotoFrameActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/PhotoFrameActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class PhotoFrameActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_frame)
+
+        val size = intent.getIntExtra("photoListSize", 0)
+        for (i in 0..size) {
+            intent.getStringExtra("photo$i")?.let {
+
+            }
+        }
     }
 }
