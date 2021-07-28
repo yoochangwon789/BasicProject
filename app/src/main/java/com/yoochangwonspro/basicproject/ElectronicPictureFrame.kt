fb8e62778302f644f6faebd9016cce085fe775b4
CN <ckddnjs554@naver.com>
2021-07-28 20:02:20 +0900

    Feat : ElectronicPictureFrame 권한 요청 when 의 else 부분 구현

--- app/src/main/java/com/yoochangwonspro/basicproject/ElectronicPictureFrame.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/ElectronicPictureFrame.kt
@@ -1,8 +1,10 @@
 package com.yoochangwonspro.basicproject
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.content.ContextCompat
 
 class ElectronicPictureFrame : AppCompatActivity() {
 
@@ -23,7 +25,30 @@ class ElectronicPictureFrame : AppCompatActivity() {
     }
 
     private fun initAddPhotoButton() {
+        addPhotoButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+                -> {
+                    // 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                -> {
+                    // 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
+                    showPermissionContextPopup()
+                }
+                else -> {
+                    // 위의 조건이 걸리지 않은 경우 앱을 처음 실행시켰을 때? 바로 권한 요청 팝업을 띄워준다
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                }
+            }
+        }
+    }
 
+    private fun showPermissionContextPopup() {
+        TODO("Not yet implemented")
     }
 
     private fun initStartPhotoFrameModeButton() {
