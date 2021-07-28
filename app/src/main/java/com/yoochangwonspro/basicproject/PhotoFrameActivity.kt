de88e859cdc2c84190482fc07c36e92f00312f77
CN <ckddnjs554@naver.com>
2021-07-28 21:45:50 +0900

    Feat : PhotoFrameActivity ImageView 타입 전역 변수 추가 작업

--- app/src/main/java/com/yoochangwonspro/basicproject/PhotoFrameActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/PhotoFrameActivity.kt
@@ -3,10 +3,19 @@ package com.yoochangwonspro.basicproject
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class PhotoFrameActivity : AppCompatActivity() {
 
     private val photoList = mutableListOf<Uri>()
+
+    private val photoImageView: ImageView by lazy {
+        findViewById(R.id.photo_frame_image_view)
+    }
+
+    private val backgroundPhotoImageView: ImageView by lazy {
+        findViewById(R.id.photo_frame_background_image_view)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +32,5 @@ class PhotoFrameActivity : AppCompatActivity() {
             }
         }
     }
-
 
 }
