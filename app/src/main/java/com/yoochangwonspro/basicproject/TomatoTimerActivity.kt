970f77ac45b25683adcd4a12f69d0085b94b74cb
CN <ckddnjs554@naver.com>
2021-07-30 22:00:43 +0900

    Feat : TomatoTimerActivity View 전역 변수 타입 설정

--- app/src/main/java/com/yoochangwonspro/basicproject/TomatoTimerActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/TomatoTimerActivity.kt
@@ -2,8 +2,23 @@ package com.yoochangwonspro.basicproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class TomatoTimerActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val remainSecondsTextView: TextView by lazy {
+        findViewById(R.id.remainSecondsTextView)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tomato_timer)
