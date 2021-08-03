cedab1545810eb745f62aefe9944d411a45ef682
CN <ckddnjs554@naver.com>
2021-08-03 21:09:08 +0900

    Feat : RecorderActivity recodeButton 의 필요한 전역 변수 초기 셋팅

--- app/src/main/java/com/yoochangwonspro/basicproject/RecorderActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/RecorderActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class RecorderActivity : AppCompatActivity() {
+
+    private val recodeButton: RecodeButton by lazy {
+        findViewById(R.id.recorde_btn)
+    }
+
+    private var state = State.BEFORE_RECORDING
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recorder)
     }
+
+
 }
