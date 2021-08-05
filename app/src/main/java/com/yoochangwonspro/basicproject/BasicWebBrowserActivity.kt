44a68ffd433f761a584fdeef87755be4a669cd4d
CN <ckddnjs554@naver.com>
2021-08-05 19:52:14 +0900

    Feat : BasicWebBrowserActivity webView 프로퍼티 지정

--- app/src/main/java/com/yoochangwonspro/basicproject/BasicWebBrowserActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicproject/BasicWebBrowserActivity.kt
@@ -2,8 +2,14 @@ package com.yoochangwonspro.basicproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class BasicWebBrowserActivity : AppCompatActivity() {
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.web_view)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basie_web_browser)
